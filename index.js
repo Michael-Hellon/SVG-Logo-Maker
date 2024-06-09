0e7f2b336e45fb04580f31696a3bb78a2a3efaed
Michael Hellon <kansasboy308@gmail.com>
2024-06-09 18:21:31 -0500

    debugged file Changes to be committed: 	new file:   .vscode/settings.json 	new file:   examples/logo_1.svg 	new file:   examples/logo_2.svg 	modified:   index.js 	modified:   lib/shapes.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Triangle, Square, Circle } = require('./lib/shapes.js');
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-const questions = [
-  {
-    type: 'list',
-    message: `Select the logo's shape:`,
-    choices: ['Triangle', 'Square', 'Circle'],
-    name: 'shape',
-  },
-  {
-    type: 'type',
-    message: 'Please Enter a hexadecimal number for the background color of the shape:',
-    name: "shapeColor"
-  },
-  {
-    type: 'type',
-    message: `Please enter up to 3 characters for the logo:`,
-    name: 'text'
-  },
-  {
-    type: 'type',
-    message: 'Please Enter a hexadecimal number for the color of the three characters:',
-    name: "textColor"
-  },
-]
+function writeToFile(fileName, answers) {
 
-function writeToFile(answers) {
-  let svgLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shapeColor}_${shape}_${textColor}_${text} </svg>`;
-  let chosenShape;  
-  switch (shapeChoice) {
+  const chosenShape = answers.shape;
+
+  let svgLogo = '';
+  svgLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+
+    let svgShape;
+  switch (chosenShape) {
       case 'Triangle':
-        chosenShape = new Triangle();
-        svgLogo +=`<polygon points="150, 20 223, 182 56, 182" fill="${answers.color}">`
+        svgShape = new Triangle();
+        svgLogo += `<polygon points="150, 20 223, 182 56, 182" fill="${answers.color}">`;
+        break;
       case 'Square':
-        chosenShape = new Square();
-        svgLogo +=`<rect x="10" y="10" width="200" height="200" fill="${answers.color}">`
+        svgShape = new Square();
+        svgLogo += `<rect x="10" y="10" width="200" height="200" fill="${answers.color}">`;
+        break;
       case 'Circle':
-        chosenShape = new Circle();
-        svgLogo +=`<circle cx="150" cy="100" r="80" fill="${answers.color}">`
-  }
-  svgLogo +=  `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}"."${answers.text}"  `
+        svgShape = new Circle();
+        svgLogo += `<circle cx="150" cy="100" r="80" fill="${answers.color}">`;
+        break;
+      }
+  
+  svgLogo += `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`
+
+  fs.writeFile(fileName, svgLogo, (err) => {
+    err ? console.log(err) : console.log("Your new logo.svg has been created!!!");
+  });
 }
 
-
 function questionUser() {
-  promise(questions)
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: `Select the logo's shape:`,
+        choices: ['Triangle', 'Square', 'Circle'],
+        name: 'shape',
+      },
+      {
+        type: 'input',
+        message: 'Please Enter a hexadecimal number for the background color of the shape:',
+        name: "shapeColor",
+        default: '#7851a9',
+      },
+      {
+        type: 'input',
+        message: `Please enter up to 3 characters for the logo:`,
+        name: 'text',
+        default: 'MWH',
+      },
+      {
+        type: 'input',
+        message: 'Please Enter a hexadecimal number for the color of the three characters:',
+        name: "textColor",
+        default: '#F5BD02'
+      },
+    ] )
 
     .then((answers) => {
         // checks that text is not more than 3 characters
-        if(answers.text.length >3) {
-          console.log('Error! Enter 3 characters at most!')
-          prompt(questions)
+        if(answers.text.length > 3) {
+          console.log('Error! Enter 3 characters at most!');
+          questionUser();
         } else {
-          writeToFile("logo.svg", svgLogo, (err) => {
-            err ? console.log(err) : console.log("Your new logo.svg has been created!!!")
-          });
-        };
-    });
-};
+          writeToFile("logo.svg", answers);
+        }
+      });
+}
 
 questionUser();
 
