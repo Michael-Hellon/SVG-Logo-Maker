4e233d5252f59c4177a38f5f6f37796a8e0eeba4
Michael Hellon <kansasboy308@gmail.com>
2024-06-08 19:30:13 -0500

    worked on main code in index.js Changes to be committed: 	modified:   index.js 	modified:   lib/test/shapes.test.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// taken from MOD10 mini-project
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer');
+const fs = require('fs')
+import { Triangle, Circle, Square} from './lib/shapes.js';
 
-const cli = new CLI();
+const questions = [
+  {
+    type: 'list',
+    message: `Select the logo's shape:`,
+    choices: ['Triangle', 'Square', 'Circle'],
+    name: 'shape',
+  },
+  {
+    type: 'type',
+    message: 'Please Enter a hexadecimal number for the background color of the shape:',
+    name: "shapeColor"
+  },
+  {
+    type: 'type',
+    message: `Please enter three characters for the logo:`,
+    name: 'text'
+  },
+  {
+    type: 'type',
+    message: 'Please Enter a hexadecimal number for the color of the three characters:',
+    name: "textColor"
+  },
+]
 
-cli.run();
