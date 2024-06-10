b166857ff000e30015be3b94b606c026e45ea2d6
Michael Hellon <kansasboy308@gmail.com>
2024-06-09 20:20:02 -0500

    debugged and SVG examples / screenshots uploaded Changes to be committed: 	deleted:    assets/circle.svg 	deleted:    examples/logo_1.svg 	deleted:    examples/logo_2.svg 	new file:   examples/logo_Gold_Circle.svg 	new file:   examples/logo_Gold_Triablge.svg 	new file:   examples/logo_Red_Square.svg 	new file:   images/logo_Gold_Circle.png 	new file:   images/logo_Gold_Triangle.png 	new file:   images/logo_Red_Square.png 	new file:   images/screenshot_test_results.png 	modified:   index.js 	modified:   lib/shapes.js 	modified:   lib/test/shapes.test.js

--- index.js
+++ index.js
@@ -7,28 +7,35 @@ function writeToFile(fileName, answers) {
   const chosenShape = answers.shape;
 
   let svgLogo = '';
-  svgLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+  svgLogo = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svgLogo += '<g>';
 
-    let svgShape;
+  console.log('answers.shape:', answers.shape)
+  svgLogo += `${answers.shape}`;
+
+  let svgShape;
   switch (chosenShape) {
-      case 'Triangle':
-        svgShape = new Triangle();
-        svgLogo += `<polygon points="150, 20 223, 182 56, 182" fill="${answers.color}">`;
-        break;
-      case 'Square':
-        svgShape = new Square();
-        svgLogo += `<rect x="10" y="10" width="200" height="200" fill="${answers.color}">`;
-        break;
-      case 'Circle':
-        svgShape = new Circle();
-        svgLogo += `<circle cx="150" cy="100" r="80" fill="${answers.color}">`;
-        break;
-      }
+    case 'Triangle':
+      svgShape = new Triangle();
+      svgLogo += `<polygon points="150,20 223,182 56,182" fill="${answers.shapeColor}"/>`;
+      break;
+    case 'Square':
+      svgShape = new Square();
+      svgLogo += `<rect x="45" y="40" width="200" height="200" fill="${answers.shapeColor}"/>`;
+      break;
+    case 'Circle':
+      svgShape = new Circle();
+      svgLogo += `<circle cx="200" cy="200" r="80" fill="${answers.shapeColor}"/>`;
+      break;
+  }
   
-  svgLogo += `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`
+  svgLogo += `<text x="150" y="150" font-size="50" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
+
+  svgLogo += '</g>';
+  svgLogo += '</svg>';
 
   fs.writeFile(fileName, svgLogo, (err) => {
-    err ? console.log(err) : console.log("Your new logo.svg has been created!!!");
+    err ? console.log(err) : console.log("Generated logo.svg!!!");
   });
 }
 
@@ -43,21 +50,18 @@ function questionUser() {
       },
       {
         type: 'input',
-        message: 'Please Enter a hexadecimal number for the background color of the shape:',
+        message: 'Please enter a hexadecimal number or a color name for the background color of the shape:',
         name: "shapeColor",
-        default: '#7851a9',
       },
       {
         type: 'input',
         message: `Please enter up to 3 characters for the logo:`,
         name: 'text',
-        default: 'MWH',
       },
       {
         type: 'input',
-        message: 'Please Enter a hexadecimal number for the color of the three characters:',
+        message: 'Please Enter a hexadecimal number or a color name for the color of the three characters:',
         name: "textColor",
-        default: '#F5BD02'
       },
     ] )
 
